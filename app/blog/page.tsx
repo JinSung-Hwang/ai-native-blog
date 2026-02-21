4732d51a1b3249b36ee710dbbd3179d455910a70
JinSung-Hwang <wlstjd7782@gmail.com>
2026-02-21 15:27:36 +0900

    브랜치 충돌

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,6 +10,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h2 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h2>
       <BlogPosts />
     </section>
   )
